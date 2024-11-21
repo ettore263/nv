e6f13fcc1e2f763c53967d3a46c9feef90e305c1
ettore263 <fernandesettoreaa@gmail.com>
2024-11-21 14:22:33 -0300

    Add files via upload

---
+++ cards/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Bankai',
+    'Quantos anos leva para se poder usar a bankai em bleach?',
+    'Cerca de dez anos de treinamento individual para ser executado.'
+)
+
+criaCartao(
+    'O Rei dos Quincys',
+    'Quem é o rei dos Quincys?',
+    'Filho do Grande Rei das Almas, Yhwach é chamado e apresentado como Rei Quincy.'
+)
+
+criaCartao(
+    'Soul Society',
+    'Quatas divisões de defesa existem na Soul Society?',
+    'Existem 13 divisões incleuindo a divisão zero.'
+)
+
+criaCartao(
+    'A Bankai',
+    'Qual a bankai mais forte de bleach?',
+    'A bankai mais forte é a de yamamoto a Zenka No Tachi'
+)
